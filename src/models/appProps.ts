75f0ca3ef6e1dec08735f77d99be0c9a3a55726c
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-01-20 02:44:50 +0300

    feat: del unnessesary files

--- src/models/appProps.ts
+++
@@ -1,9 +0,0 @@
-import Component from '../base/Component';
-import AppComponent from '../components/AppComponent';
-import Tags from '../enums/Tags';
-
-export type InitAppProps = {
-  app: AppComponent;
-  header: Component<Tags.header>;
-  main: Component<Tags.main>;
-};
