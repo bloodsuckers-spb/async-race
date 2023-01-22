5c251c556dfef5f697953853af19fc5475bffbde
bloodsuckers-spb <bloodsuckers-spb@hotmail.com>
2023-01-22 03:31:44 +0300

    feat: update CreateCarForm

--- src/components/CreateCarForm/index.ts
+++ src/components/CreateCarForm/index.ts
@@ -20,40 +20,39 @@ class CreateCarForm extends Component<Tags.form> {
     this.btn = btn;
     this.append(textInput, colorInput, btn);
 
-    this.textInput.node.oninput = () => {
-      const { value } = this.textInput.node;
-      const { node } = this.btn;
-      this.textInput.node.value = value.trim();
-      if (!node.disabled) return;
-      node.disabled = false;
-    };
-
-    this.btn.node.onclick = (): false => {
-      this.btn.node.disabled = true;
-
-      if (!this.textInput.node.value) {
-        return false;
-      }
-
-      const nameValue = this.textInput.node.value;
-      const colorValue = this.colorInput.node.value;
-
-      const data = { name: nameValue, color: colorValue };
-
-      this.load({
-        method: RequestMethods.post,
-        queryString: `${API.baseLink}/garage`,
-        eventName: CustomEvents.createNewCar,
-        options: {
-          body: JSON.stringify(data),
-        },
-      });
-
-      this.textInput.node.value = '';
-
-      return false;
-    };
+    this.textInput.node.oninput = () => this.handleOnInput();
+    this.btn.node.onclick = () => this.handleClick();
   }
+
+  private handleClick = () => {
+    this.btn.node.disabled = true;
+
+    if (!this.textInput.node.value) {
+      return false;
+    }
+
+    this.load({
+      method: RequestMethods.post,
+      queryString: `${API.baseLink}/garage`,
+      eventName: CustomEvents.createNewCar,
+      options: {
+        name: this.textInput.node.value,
+        color: this.colorInput.node.value,
+      },
+    });
+
+    this.textInput.node.value = '';
+
+    return false;
+  };
+
+  private handleOnInput = () => {
+    const { value } = this.textInput.node;
+    const { node } = this.btn;
+    this.textInput.node.value = value.trim();
+    if (!node.disabled) return;
+    node.disabled = false;
+  };
 }
 
 export default CreateCarForm;
